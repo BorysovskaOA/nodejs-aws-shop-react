d7af920124d5b96d76bd56b64ec45f37031c8e5a
BorysovskaOA <olya.borysovska@gmail.com>
2026-05-16 17:08:03 +0300

    Connected import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://c176ziepa0.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://qyy6zuiyrl.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
